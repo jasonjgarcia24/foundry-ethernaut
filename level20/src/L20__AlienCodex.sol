2ac3028953fe251cd5afab23c0269c2f7c8b1e61
jjgarcia.eth <jason.garcia24@gmail.com>
2023-06-20 14:01:56 -0500

    Level 20 cleanup.

--- level20/src/L20__AlienCodex.sol
+++ level20/src/L20__AlienCodex.sol
@@ -1,7 +1,91 @@
-// SPDX-License-Identifier: MIT
+/**
+ *  SourceUnit: /home/jason/Documents/education/Ethernaut/foundry-ethernaut/level20/src/L20__AlienCodex.sol
+ */
+
 pragma solidity ^0.5.0;
 
-import "./Ownable-05.sol";
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @return the address of the owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+     * @return true if `msg.sender` is the owner of the contract.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Allows the current owner to relinquish control of the contract.
+     * @notice Renouncing to ownership will leave the contract without an owner.
+     * It will not be possible to call the functions with the `onlyOwner`
+     * modifier anymore.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ *  SourceUnit: /home/jason/Documents/education/Ethernaut/foundry-ethernaut/level20/src/L20__AlienCodex.sol
+ */
+
+////// SPDX-License-Identifier-FLATTEN-SUPPRESS-WARNING: MIT
+pragma solidity ^0.5.0;
+
+////import "./Ownable-05.sol";
 
 contract AlienCodex is Ownable {
     bool public contact;
@@ -28,5 +112,3 @@ contract AlienCodex is Ownable {
         codex[i] = _content;
     }
 }
-
-// 0x00000000000000000000000090CfBFFb21423219A6BcDbE08f8dC108Ca7EEA75
